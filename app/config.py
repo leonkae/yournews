9bbdcec0f00c698a49b9aafecc23e06a6e3cad11
leonkae <leon29kae@gmail.com>
2022-05-03 12:10:24 +0300

    modified config

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ from flask import Config
 
 
 class config:
+    '''holds base urls'''
     SOURCE_BASE_URL = "https://newsapi.org/v2/top-headlines/sources?apiKey={}"
     NEWS_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
  # https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=444f55e941ed44459db54db9932d8905
@@ -9,5 +10,6 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
+    '''holds debugger'''
     DEBUG = True
     
