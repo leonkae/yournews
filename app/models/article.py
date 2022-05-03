0de838714fff0aff0a017f1b51bc0c9e393f7b92
leonkae <leon29kae@gmail.com>
2022-05-03 08:53:29 +0300

    files updated

---
+++ app/models/article.py
@@ -0,0 +1,11 @@
+class Article:
+    
+    def __init__(self,author,title,description,url,urlToImage,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.content = content
+        
+        # https://cdn.onemileatatime.com/wp-content/uploads/2022/03/United-Airlines-787-Business-Class-17-1.jpeg?width=1200&auto_optimize=low&quality=75&height=900&aspect_ratio=4%3A3
---
+++ app/models/source.py
@@ -0,0 +1,9 @@
+class Source:
+    source = []
+    
+    def __init__(self,name,url):
+        self.name = name
+        self.url = url
+    
+    
+    
