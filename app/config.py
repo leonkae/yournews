d535b82acd633e5de057a5cf3f41b3b720df3343
leonkae <leon29kae@gmail.com>
2022-05-03 08:49:57 +0300

    modified config file

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from flask import Config
+
+
+class config:
+    SOURCE_BASE_URL = "https://newsapi.org/v2/top-headlines/sources?apiKey={}"
+    NEWS_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
+ # https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=444f55e941ed44459db54db9932d8905
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+    
